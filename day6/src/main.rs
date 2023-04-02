748410a4adec6abf3f86620d744e2804d15aeb02
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-04-01 20:38:26 -0500

    day6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,11 +1,23 @@
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    println!("{:?}", parse_input(&input));
+    println!("{:?}", part1(&input));
+    println!("{:?}", part2(&input));
 }
 
-fn parse_input(input: &String) {
-    print!("{:?}", input);
-    for char in input.chars() {
-        print!("{:?}", char);
-    }
+fn part1(input: &String) -> usize {
+    input
+        .as_bytes()
+        .windows(4)
+        .position(|v| !(0..3).any(|i| (i + 1..4).any(|j| v[i] == v[j])))
+        .unwrap()
+        + 4
+}
+
+fn part2(input: &String) -> usize {
+    input
+        .as_bytes()
+        .windows(14)
+        .position(|v| !(0..13).any(|i| (i + 1..14).any(|j| v[i] == v[j])))
+        .unwrap()
+        + 14
 }
