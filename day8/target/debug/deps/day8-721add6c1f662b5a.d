f26d5a2a4d468b1affcb0dc1084a9b2484b09123
Thomas <tgusewelle@gkwmedia.com>
2023-04-05 14:57:34 -0500

    day 8 init

---
+++ day8/target/debug/deps/day8-721add6c1f662b5a.d
@@ -0,0 +1,5 @@
+/home/tgusewelle/Dev/aoc-learning-rust/day8/target/debug/deps/day8-721add6c1f662b5a.rmeta: src/main.rs
+
+/home/tgusewelle/Dev/aoc-learning-rust/day8/target/debug/deps/day8-721add6c1f662b5a.d: src/main.rs
+
+src/main.rs:
---
+++ day8/target/debug/deps/day8-882cdbd94dbadf46.d
@@ -0,0 +1,5 @@
+/home/tgusewelle/Dev/aoc-learning-rust/day8/target/debug/deps/day8-882cdbd94dbadf46.rmeta: src/main.rs
+
+/home/tgusewelle/Dev/aoc-learning-rust/day8/target/debug/deps/day8-882cdbd94dbadf46.d: src/main.rs
+
+src/main.rs:
